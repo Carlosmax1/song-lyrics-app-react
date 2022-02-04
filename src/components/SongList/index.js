443674f7bc8ff2f313c4536ab5bcb2d88df105ce
Carlos Eduardo <68128063+Carlosmax1@users.noreply.github.com>
2022-02-04 00:26:28 -0300

    implementacao do botao Ver Letra e mudança na imagem do cabecalho

--- src/components/SongList/index.js
+++ src/components/SongList/index.js
@@ -1,16 +1,44 @@
 import react from "react";
-import './index.css'
+import { useEffect, useState } from "react";
+import apiClient from "../../config/http";
+import './index.css';
 
 export default function SongList({ songsList }) {
+
+	const [key, setKey] = useState(null);
+	const [lyrics, setLyrics] = useState(null);
+
+	const getLyric = async (songsList, key) => {
+		const res = apiClient.get(`v1/${songsList[key].artist.name}/${songsList[key].title}`)
+		return res;
+	}
+
+	useEffect(() => {
+		const lyrics = async () => {
+			let list = await getLyric(songsList, key);
+			setLyrics(list);
+		}
+		lyrics();
+	}, [key, songsList])
+
+	console.log(lyrics);
 
 	return (
 		<>
 			<div className="songs-container">
 				{songsList.map((item, key) => (
 					<li key={key} className="songs">{item.artist.name} - {item.title}
-						<button className="botao-letra">Ver letra</button>
+						<button onClick={() => setKey(key)} className="botao-letra">Ver letra</button>
 					</li>
 				))}
+				{lyrics !== null && (
+					<>
+						<div className="letra-container">
+						<span className="letra">{lyrics.data.lyrics}</span>
+						</div>
+					</>
+				)}
+
 			</div>
 		</>
 	)
